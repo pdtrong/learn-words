fc865beebe7f4a6799c4dd43417daacb625860bd
trongpham_fiot <trongpham@fiot.vn>
2020-03-10 21:49:22 +0700

    add project

---
+++ app/main.py
@@ -0,0 +1,110 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from repeat_timer import RepeatedTimer
+import sys
+import random
+
+
+class MyWord(object):
+    def __init__(self):
+        self.word_list = list()
+
+    def set_word_list(self, file_path):
+        with open(file_path, 'r') as file:
+            self.word_list = file.readlines()
+
+        self.word_list = [x.replace('\n', '').replace('\r', '').strip()
+                          for x in self.word_list]
+
+    def get_random_word(self):
+        if not len(self.word_list):
+            return ''
+        else:
+            return random.choice(self.word_list)
+
+
+class MyApp(QWidget):
+    trigger_update_word = pyqtSignal(dict)
+
+    def __init__(self):
+        super().__init__()
+
+        self.trigger_update_word.connect(self.print_word)
+
+        self.setWindowTitle('Words')
+
+        self.lbl_print_word = QLabel()
+        self.lbl_print_word.setText('Hello World!!!')
+        self.lbl_print_word.setFont(QFont('Arial', 24, QFont.Bold))
+        self.lbl_print_word.setAlignment(Qt.AlignCenter)
+
+        self.btn_import_word = QPushButton()
+        self.btn_import_word.setText('Import')
+        self.btn_import_word.clicked.connect(self.import_word_list)
+
+        self.btn_print_word = QPushButton()
+        self.btn_print_word.setText('Print')
+
+        layout = QGridLayout(self)
+        param = [self.lbl_print_word, 0, 0, 1, 2]
+        layout.addWidget(*param)
+        param = [self.btn_import_word, 1, 0]
+        layout.addWidget(*param)
+        param = [self.btn_print_word, 1, 1]
+        layout.addWidget(*param)
+
+        self.setLayout(layout)
+        self.setWindowIcon(QIcon('./logo.ico'))
+
+        self.my_word = MyWord()
+        self.my_repeat_timer = None
+        self.start_timer()
+
+    def start_timer(self):
+        self.my_repeat_timer = RepeatedTimer(5, self.print_word, {})
+        self.my_repeat_timer.start()
+
+    @pyqtSlot()
+    def import_word_list(self):
+        file_info = QFileDialog.getOpenFileName()
+        self.my_word.set_word_list(file_info[0])
+
+    @pyqtSlot(dict)
+    def print_word(self, result):
+        word = self.my_word.get_random_word()
+        self.lbl_print_word.setText(word)
+
+    def close_app(self):
+        self.my_repeat_timer and self.my_repeat_timer.stop()
+        print('closed app')
+
+
+if __name__ == '__main__':
+    # Create object
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+
+    # Now use a palette to switch to dark colors:
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.white)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(palette)
+
+    my_app = MyApp()
+    my_app.show()
+
+    # Handle event quit
+    app.aboutToQuit.connect(my_app.close_app)
+    sys.exit(app.exec_())
