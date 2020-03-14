300fca7e39be35fffb78bdb64f4066c48e2e0261
trongpham_fiot <trongpham@fiot.vn>
2020-03-15 00:48:21 +0700

    add submenu

--- app/main.py
+++ app/main.py
@@ -23,34 +23,46 @@ class MainWindow(QMainWindow):
     trigger_update_word_list = pyqtSignal(dict)
     trigger_print_about = pyqtSignal(dict)
     trigger_setup_timer = pyqtSignal(dict)
+    trigger_choice_random = pyqtSignal(dict)
+    trigger_choice_order = pyqtSignal(dict)
 
     def __init__(self, parent=None):
         super().__init__(parent, Qt.WindowFlags())
-        self.setWindowTitle('Learn words')
+
+        # ------------------------------------------------------------
+        self.submenu_choice = dict()
 
         # ------------------------------------------------------------
         self.wg_my_app = MyApp(parent=self)
-        self.setCentralWidget(self.wg_my_app)
 
         height, width = 200, 400
         center_point = QDesktopWidget().availableGeometry().center()
+
         self.setGeometry(int(center_point.x() - width / 2), int(center_point.y() - height / 2), width, height)
+        self.setCentralWidget(self.wg_my_app)
+        self.setWindowTitle('Learn words')
 
         # ------------------------------------------------------------
         self.trigger_update_stay_on_top.connect(self.update_stay_op_top_event)
         self.trigger_update_word_list.connect(self.wg_my_app.import_word_list_event)
         self.trigger_print_about.connect(self.print_about_event)
         self.trigger_setup_timer.connect(self.setup_timer_event)
+        self.trigger_choice_random.connect(self.choice_word_event)
+        self.trigger_choice_order.connect(self.choice_word_event)
 
         # ------------------------------------------------------------
         self.menu_bar = self.menuBar()
 
+        # ------------------------------------------------------------
         # File
         mb_file = self.menu_bar.addMenu('File')
+
+        # # Import words
         qa_import_word_list = QAction('Import words', self)
         qa_import_word_list.triggered.connect(lambda ign: self.trigger_update_word_list.emit({}))
         mb_file.addAction(qa_import_word_list)
 
+        # ------------------------------------------------------------
         # Edit
         mb_edit = self.menu_bar.addMenu('Edit')
 
@@ -58,17 +70,41 @@ class MainWindow(QMainWindow):
         qa_setup_timer = QAction('Adjust timer', self)
         qa_setup_timer.triggered.connect(lambda ign: self.trigger_setup_timer.emit({}))
 
-        mb_edit.addAction(qa_setup_timer)
+        # # Choice
+        mn_choice_word = QMenu('Choice', self)
 
+        # # # Random
+        qa_choice_random = QAction('Random', self, checkable=True)
+        qa_choice_random.setChecked(True)
+        qa_choice_random.triggered.connect(lambda ign: self.trigger_choice_random.emit({'name': 'Random'}))
+        mn_choice_word.addAction(qa_choice_random)
+        self.submenu_choice['Random'] = qa_choice_random
+
+        # # # Order
+        qa_choice_order = QAction('Order', self, checkable=True)
+        qa_choice_order.setChecked(False)
+        qa_choice_order.triggered.connect(lambda ign: self.trigger_choice_order.emit({'name': 'Order'}))
+        mn_choice_word.addAction(qa_choice_order)
+        self.submenu_choice['Order'] = qa_choice_order
+
+        mb_edit.addAction(qa_setup_timer)
+        mb_edit.addMenu(mn_choice_word)
+
+        # ------------------------------------------------------------
         # Window
         mb_window = self.menu_bar.addMenu('Window')
+
+        # # Stay on top
         qa_stay_on_top = QAction('Stay on top', self, checkable=True)
         qa_stay_on_top.setChecked(False)
         qa_stay_on_top.triggered.connect(lambda state: self.trigger_update_stay_on_top.emit({'state': state}))
         mb_window.addAction(qa_stay_on_top)
 
+        # ------------------------------------------------------------
         # Help
         mb_help = self.menu_bar.addMenu('Help')
+
+        # # About
         qa_print_about = QAction('About', self)
         qa_print_about.triggered.connect(lambda ign: self.trigger_print_about.emit({}))
         mb_help.addAction(qa_print_about)
@@ -98,6 +134,10 @@ class MainWindow(QMainWindow):
         '''
         parameter = [self, 'About', about_me]
         QMessageBox.about(*parameter)
+
+    @pyqtSlot(dict)
+    def choice_word_event(self, result):
+        [self.submenu_choice[x].setChecked(result['name'] == x) for x in self.submenu_choice.keys()]
 
     def close_app(self):
         self.wg_my_app.my_repeat_timer and self.wg_my_app.my_repeat_timer.stop()
