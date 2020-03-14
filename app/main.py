56c08e9bf42269bbe89e0fa4eadf0bafa68efae5
trongpham_fiot <trongpham@fiot.vn>
2020-03-14 12:05:22 +0700

    refactor

--- app/main.py
+++ app/main.py
@@ -7,6 +7,12 @@ import os
 import sys
 import ctypes
 
+
+if hasattr(sys, '_MEIPASS'):
+    print('running in a PyInstaller bundle')
+else:
+    print('running in a normal Python process')
+
 DEFAULT_STR = '¯\_(ツ)_/¯'
 
 
@@ -187,7 +193,7 @@ if __name__ == '__main__':
 
     # ------------------------------------------------------------
     # Create object
-    path_icon = get_root_folder() + '/logo.png'
+    path_icon = get_root_folder() + '/logo.ico'
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
     app.setWindowIcon(QIcon(path_icon))
